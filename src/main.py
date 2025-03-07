fc19503eb60577b8bc777f6454957a6b19611302
Tolga <mail@tolgadur.com>
2025-03-07 18:16:58 +0000

    mps doesnt work with this train setup

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
-from trainer import regular_train, distillation_train
+# from trainer import regular_train, distillation_train
+from utils import load_model, generate_text
 
 
 def main():
     # You can choose which training method to use by uncommenting one of these:
-    distillation_train(epochs=10)
-    regular_train(epochs=10)
+    # distillation_train(epochs=10)
+    # regular_train(epochs=10)
+
+    # Inference
+    model = load_model("models/distillation_model_7.pth")
+    text = generate_text(model)
+    print(text)
 
 
 if __name__ == "__main__":
